e16b49787dab6430e40702c57c17db7b456108e8
ryskit <ryosukekida.71@gmail.com>
2019-06-20 18:46:38 +0900

    implements arithmetic operations

--- 9cc.c
+++ 9cc.c
@@ -1,5 +1,188 @@
+#include <ctype.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+enum {
+  TK_NUM = 256,
+  TK_EOF,
+};
+
+typedef struct {
+  int ty;
+  int val;
+  char *input;
+} Token;
+
+char *user_input;
+
+Token tokens[100];
+
+int pos = 0;
+
+void error(char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+// エラー箇所を報告するための関数
+void error_at(char *loc, char *msg) {
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+  fprintf(stderr, "^ %s\n", msg);
+  exit(1);
+}
+
+void tokenize() {
+  char *p = user_input;
+
+  int i = 0;
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
+      tokens[i].ty = *p;
+      tokens[i].input = p;
+      i++;
+      p++;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      tokens[i].ty = TK_NUM;
+      tokens[i].input = p;
+      tokens[i].val = strtol(p, &p, 10);
+      i++;
+      continue;
+    }
+
+    error_at(p, "トークナイズできません");
+  }
+
+  tokens[i].ty = TK_EOF;
+  tokens[i].input = p;
+}
+
+enum {
+  ND_NUM = 256,     // 整数のノードの型
+};
+
+typedef struct Node {
+  int ty;           // 演算子かND_NUM
+  struct Node *lhs; // 左辺
+  struct Node *rhs; // 右辺
+  int val;          // tyがND_NUMの場合のみ使う
+} Node;
+
+int consume(int ty) {
+  if (tokens[pos].ty != ty)
+    return 0;
+  pos++;
+  return 1;
+}
+
+Node *new_node(int ty, Node *lhs, Node *rhs) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ty;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = malloc(sizeof(Node));
+  node->ty = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *mul();
+
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node('+', node, mul());
+    else if (consume('-'))
+      node = new_node('-', node, mul());
+    else
+      return node;
+  }
+}
+
+Node *term();
+
+Node *mul() {
+  Node *node = term();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_node('*', node, term());
+    else if (consume('/'))
+      node = new_node('/', node, term());
+    else
+      return node;
+  }
+}
+
+Node *term() {
+  // 次のトークンが'('なら、"(" expr ")"のはず
+  if (consume('(')) {
+    Node *node = expr();
+    if (!consume(')'))
+      error_at(tokens[pos].input,
+          "開きカッコに対応する閉じカッコがありません");
+    return node;
+  }
+
+  // そうでなければ数値のはず
+  if (tokens[pos].ty == TK_NUM)
+    return new_node_num(tokens[pos++].val);
+
+  error_at(tokens[pos].input,
+      "数値でも開きカッコでもないトークンです");
+  return NULL; //
+}
+
+void gen(Node *node) {
+  if (node->ty == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty) {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  case '*':
+    printf("  imul rdi\n");
+    break;
+  case '/':
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+  }
+
+  printf("  push rax\n");
+}
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -7,10 +190,23 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  // トークナイズしてパースする
+  user_input = argv[1];
+  tokenize();
+  Node *node = expr();
+
+  // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".global _main\n");
   printf("_main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
+
+  // 抽象構文木を下りながらコード生成
+  gen(node);
+
+  // スタックトップに式全体の値が残っているはずなので
+  // それをRAXにロードして関数からの返り値とする
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
+
